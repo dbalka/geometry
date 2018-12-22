872892fb798e2362e34e6b4234a76a10c3c2723e
levabala <levabala@ya.ru>
2018-12-22 21:26:25 +0300

    fps

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+
+var canvas = document.getElementById('can');
+var context = canvas.getContext('2d');
+
+var w = get('w', 1000);
+canvas.width = w;
+canvas.height = w;
+var w2 = w / 2;
+var k = get('k', 200);
+var kk = get('kk', 1200);
+var n = get('num', 1.5);
+var dn = get('dn', 0.001);
+
+var opacity = get('op', 0.4);
+var thickness = get('th', 0.3);
+
+redraw(canvas, context, w, w2, k, kk, n, opacity, thickness);
+
+var delta = 1;
+var deltaSmallCoeff = dn;
+
 function getUrlVars() {
   var vars = {};
   var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (
@@ -18,21 +39,43 @@ function get(parameter, defaultvalue) {
   return urlparameter;
 }
 
+function px(j) {
+  var da = (Math.PI * 2) / k;
+  var xx = w2 + Math.sin(da * j) * w2;
+  return xx;
+}
+function py(j) {
+  var da = (Math.PI * 2) / k;
+  var yy = w2 + Math.cos(da * j) * w2;
+  return yy;
+}
+
+var pcx = [];
+var pcy = [];
+
+var prev_k = 0;
+var frame = 0;
+var fps = 0;
+
 function redraw(canvas, context, w, w2, k, kk, n, opacity, thickness) {
   // clear canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  var pcx = [];
-  var pcy = [];
-
-  var da = (Math.PI * 2) / k;
-
-  for (var i = 0; i < k; i++) {
-    var xx = w / 2 + Math.sin(da * i) * w2;
-    var yy = w / 2 + Math.cos(da * i) * w2;
-    pcx.push(xx);
-    pcy.push(yy);
+  //if (k != prev_k) 
+  {
+    var da = (Math.PI * 2) / k;
+    pcx = [];
+    pcy = [];
+    for (var i = 0; i < k; i++) {
+      var xx = w / 2 + Math.sin(da * i) * w2;
+      var yy = w / 2 + Math.cos(da * i) * w2;
+      pcx.push(xx);
+      pcy.push(yy);
+    }
+    prev_k = k;
   }
+
+  frame++;
 
   context.lineWidth = thickness;
   context.strokeStyle = 'rgba(0,0,200,' + opacity + ')';
@@ -60,32 +103,17 @@ function redraw(canvas, context, w, w2, k, kk, n, opacity, thickness) {
   //var numst = 'n = '+n;
   var x = Math.round(n * 1000) / 1000;
   context.fillText("n = " + x, 20, 30);
-
+  context.fillText("fps = " + fps, 20, 50);
+  //context.fillText("keycode = " + keycode, 20, 45);
   var dataURL = canvas.toDataURL();
-  document.getElementById('canvasImg').src = dataURL;
+  document.getElementById('canImg').src = dataURL;
 }
 
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
+//var keycode = 0;
 
-var w = get('w', 1000);
-canvas.width = w;
-canvas.height = w;
-var w2 = w / 2;
-var k = get('k', 200);
-var kk = get('kk', 1200);
-var n = get('num', 1.5);
-var dn = get('dn', 0.001);
-
-var opacity = get('op', 0.4);
-var thickness = get('th', 0.3);
-
-redraw(canvas, context, w, w2, k, kk, n, opacity, thickness);
-
-var delta = 1;
-var deltaSmallCoeff = dn;
 window.addEventListener('keypress', e => {
   var coeff = e.shiftKey ? deltaSmallCoeff : 1;
+  //var keycode = e.code;
   switch (e.code) {
     case 'BracketRight':
       n += delta * coeff;
@@ -111,8 +139,11 @@ var signs = {
 
 function startAuto() {
   var lastStamp = Date.now();
+
   function animate() {
+
     var nowStamp = Date.now();
+
     Object.entries(deltasPerSec).forEach(entrie => {
       var delta = (entrie[1] * (nowStamp - lastStamp)) / 1000;
       window[entrie[0]] = parseFloat(window[entrie[0]]) + delta;
@@ -125,11 +156,19 @@ function startAuto() {
 }
 
 var counter = 0;
+
 function nextFrame() {
   requestAnimationFrame(nextFrame);
   if (counter % 2 == 0)
     redraw(canvas, context, w, w2, k, kk, n, opacity, thickness);
 }
+
+var prev_frame = 0;
+function oneSecTimer() {
+  fps = frame - prev_frame;
+  prev_frame = frame;
+}
+setInterval(oneSecTimer, 1000);
 
 nextFrame();
 startAuto();
